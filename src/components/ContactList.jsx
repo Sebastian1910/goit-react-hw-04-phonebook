a860d123e97dfd06b53fc38d501495988d189351
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-06-10 12:09:41 +0300

    a

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ContactListItem from "./ContactListItem";
+
+// Komponent listy kontaktów
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul>
+    {contacts.map((contact, index) => (
+      <ContactListItem
+        key={index}
+        contact={contact}
+        onDelete={() => onDeleteContact(index)}
+      />
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
---
+++ src/components/ContactListItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// Komponent pojedynczego elementu listy kontaktów
+const ContactListItem = ({ contact, onDelete }) => (
+  <li>
+    {contact.name}: {contact.number}
+    <button onClick={onDelete}>Delete</button>
+  </li>
+);
+
+ContactListItem.propTypes = {
+  contact: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default ContactListItem;
